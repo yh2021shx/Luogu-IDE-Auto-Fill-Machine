7f9dec88d42fa837c452da1c51d52d42061080b3
yh2021shx <110185564+yh2021shx@users.noreply.github.com>
2025-02-16 14:20:58 +0800

    Add Setting Page

--- code.js
+++ code.js
@@ -31,33 +31,8 @@ var darkmode = 0;
     }
     function myFunction() {
         var mamba = document.getElementsByClassName("cm-activeLine cm-line");
-        const styleSheet = document.createElement('style');
-        document.head.appendChild(styleSheet);
-        // 添加新的样式规则
-        if (darkmode == 0) {
-            styleSheet.sheet.insertRule('.ͼ5 { color: #7C4EFF; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼb { color: #7C4EFF; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼd { color: #F76D47; }', 0); // 数字
-            styleSheet.sheet.insertRule('.ͼe { color: #91B878; }', 0); // 字符串
-            styleSheet.sheet.insertRule('.ͼm { color: #CCD7DA; }', 0); // 注释
-            styleSheet.sheet.insertRule('.ͼg { color: #80CBC4; }', 0); // 函数名
-            styleSheet.sheet.insertRule('.ͼi { color: #7C4EFF; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼf { color: #91B859; }', 0); // 头文件
-            styleSheet.sheet.insertRule('.cm-editor { color: #6182B8; }', 0); // 普通字符
-        } else {
-            styleSheet.sheet.insertRule('.ͼ5 { color: #C792EA; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼb { color: #C792EA; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼd { color: #F78A68; }', 0); // 数字
-            styleSheet.sheet.insertRule('.ͼe { color: #C3E88D; }', 0); // 字符串
-            styleSheet.sheet.insertRule('.ͼm { color: #4A4A4A; }', 0); // 注释
-            styleSheet.sheet.insertRule('.ͼg { color: #7BAAFF; }', 0); // 函数名
-            styleSheet.sheet.insertRule('.ͼi { color: #C792EA; font-style: italic; }', 0); // 关键字
-            styleSheet.sheet.insertRule('.ͼf { color: #C3D365; }', 0); // 头文件
-            styleSheet.sheet.insertRule('.cm-editor { color: #EEFFFF; }', 0); // 普通字符
-            styleSheet.sheet.insertRule('.ͼ4 .cm-line { caret-color: yellow !important; }', 0); // 光标
-        }
         styleSheet.sheet.insertRule('.ͼq .cm-content { font-family: Fira Code !important; font-size: 15px;}');
-        styleSheet.sheet.insertRule('.ide-container[data-v-6198bd39] { height: calc(100vh - 3.5rem) }');
+        styleSheet.sheet.insertRule('.ide-container { height: calc(100vh - 3.5rem) }');
         var fnfnfn = document.getElementsByClassName("ͼ1");
         fnfnfn[0].style['font-family'] = "Fira Code !important";
         if (mamba[0].innerHTML != '<br>') {
@@ -87,22 +62,44 @@ var darkmode = 0;
         }
         return t;
     }
+    function LightMode() {
+        const styleSheet = document.createElement('style');
+        styleSheet.setAttribute('id', 'Lightmode-Style');
+        document.head.appendChild(styleSheet);
+        styleSheet.sheet.insertRule('.ͼ5 { color: #7C4EFF; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼb { color: #7C4EFF; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼd { color: #F76D47; }', 0); // 数字
+        styleSheet.sheet.insertRule('.ͼe { color: #91B878; }', 0); // 字符串
+        styleSheet.sheet.insertRule('.ͼm { color: #CCD7DA; }', 0); // 注释
+        styleSheet.sheet.insertRule('.ͼg { color: #80CBC4; }', 0); // 函数名
+        styleSheet.sheet.insertRule('.ͼi { color: #7C4EFF; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼf { color: #91B859; }', 0); // 头文件
+        styleSheet.sheet.insertRule('.cm-editor { color: #6182B8; }', 0); // 普通字符
+    }
     function DarkMode() {
+        let ret = localStorage.getItem('DE_aemmprty_darkmode') ;
+        if (ret == '0') {
+            LightMode();
+            return ;
+        } else if (ret == null) {
+            ret = 1;
+        }
         darkmode = 1;
         const styleSheet = document.createElement('style');
+        styleSheet.setAttribute('id', 'Darkmode-Style');
         document.head.appendChild(styleSheet);
         styleSheet.sheet.insertRule('.top-bar { background: black !important;}', 0);
         styleSheet.sheet.insertRule('.svg-inline--fa { color: white !important;} ', 0);
-        styleSheet.sheet.insertRule('.top-bar .breadcrumb[data-v-4430a926]>* { color: white !important;}', 0);
-        styleSheet.sheet.insertRule('.lside.bar[data-v-4430a926] { background: #22272E !important;}', 0);
-        styleSheet.sheet.insertRule('nav.sidebar[data-v-06583f34]:not(:hover) li .title { color: white !important;}', 0);
+        styleSheet.sheet.insertRule('.top-bar .breadcrumb>* { color: white !important;}', 0);
+        styleSheet.sheet.insertRule('.lside.bar { background: #22272E !important;}', 0);
+        styleSheet.sheet.insertRule('nav.sidebar:not(:hover) li .title { color: white !important;}', 0);
         styleSheet.sheet.insertRule('.ide-container .problem { background: #2D333B !important;}', 0);
         styleSheet.sheet.insertRule('.lfe-h1, .lfe-h2, .lfe-h3, .lfe-h4, .lfe-h5, .lfe-h6 { color: white !important;} ', 0);
         styleSheet.sheet.insertRule('.lfe-marked-wrap { color: white; } ', 0);
         styleSheet.sheet.insertRule('.lfe-marked pre, .lfe-marked pre>code { background: #22272E; color: white; } ', 0);
         styleSheet.sheet.insertRule('.ide-toolbar { background: #22272E; }', 0);
         styleSheet.sheet.insertRule('.ide-toolbar .title>.text { color: white; } ', 0);
-        styleSheet.sheet.insertRule('label[data-v-25b6974c] { color: white; } ', 0);
+        styleSheet.sheet.insertRule('label { color: white; } ', 0);
         styleSheet.sheet.insertRule('.ͼ1 .cm-gutter { background: #22272E; }', 0);
         styleSheet.sheet.insertRule('.ͼ1 .cm-content[contenteditable=true] { background: #212121; }', 0);
         styleSheet.sheet.insertRule('::-webkit-scrollbar-track { background: #282828; }', 0);
@@ -115,7 +112,7 @@ var darkmode = 0;
         styleSheet.sheet.insertRule('.ͼ2 .cm-activeLine { background: #171717; }', 0);
         styleSheet.sheet.insertRule('.ide-toolbar { border: 1px solid #22272E }', 0);
         styleSheet.sheet.insertRule('.ͼ2 .cm-gutters { border: 1px solid #22272E }', 0);
-        styleSheet.sheet.insertRule('input[data-v-6d12c5db], textarea[data-v-6d12c5db], .refined-input[data-v-6d12c5db] { border: 1px solid #22272E }', 0);
+        styleSheet.sheet.insertRule('input, textarea.ide-textarea.lfe-code, .refined-input { border: 1px solid #22272E }', 0);
         styleSheet.sheet.insertRule('.lfe-marked pre, .lfe-marked pre > code { border: 1px solid #22272E }', 0);
         styleSheet.sheet.insertRule('.lfe-code { background: #22272E; }', 0);
         styleSheet.sheet.insertRule('.lfe-code { color: white; }', 0);
@@ -127,7 +124,106 @@ var darkmode = 0;
         styleSheet.sheet.insertRule('.l-flex-info-row>*:last-child { color: white; }', 0);
         styleSheet.sheet.insertRule('.l-flex-info-row>*:first-child { color: white; }', 0);
         styleSheet.sheet.insertRule('.theme-bg[data-v-f265fec6], .theme-fg[data-v-f265fec6] { background: #2D333B; }', 0);
+        styleSheet.sheet.insertRule('.ͼ5 { color: #C792EA; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼb { color: #C792EA; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼd { color: #F78A68; }', 0); // 数字
+        styleSheet.sheet.insertRule('.ͼe { color: #C3E88D; }', 0); // 字符串
+        styleSheet.sheet.insertRule('.ͼm { color: #4A4A4A; }', 0); // 注释
+        styleSheet.sheet.insertRule('.ͼg { color: #7BAAFF; }', 0); // 函数名
+        styleSheet.sheet.insertRule('.ͼi { color: #C792EA; font-style: italic; }', 0); // 关键字
+        styleSheet.sheet.insertRule('.ͼf { color: #C3D365; }', 0); // 头文件
+        styleSheet.sheet.insertRule('.cm-editor { color: #EEFFFF; }', 0); // 普通字符
+        styleSheet.sheet.insertRule('.ͼ4 .cm-line { caret-color: yellow !important; }', 0); // 光标
         console.log("DarkMode is opened");
+    }
+    function Public() {
+        const styleSheet = document.createElement('style');
+        styleSheet.setAttribute('id', 'Public-Style');
+        document.head.appendChild(styleSheet);
+        styleSheet.sheet.insertRule('#IDE-Setting:hover { box-shadow: 0 0 7px 1px cornflowerblue }', 0);
+    }
+    function Open() {
+        var it = document.getElementsByClassName('user-nav');
+        return it.length > 0;
+    }
+    function AddSettingButton() {
+        var it = document.getElementsByClassName('user-nav')[0];
+        var nw = document.createElement('div');
+        nw.setAttribute('id', 'IDE-Setting');
+        nw.style.margin = '10px';
+        nw.style.display = 'inline-block';
+        nw.style.color = 'white';
+        nw.style.background = 'cornflowerblue';
+        nw.style.padding = '8px 10px 8px 10px';
+        nw.style.transition = 'box-shadow 0.3s';
+        nw.style['border-radius'] = '5px';
+        //nw.style['box-shadow'] = '0 0 7px 1px white';
+        nw.style['font-family'] = 'Fira Code, Fira Mono, Consolas';
+        nw.style['font-size'] = '15px';
+        nw.innerHTML = 'Setting';
+        it.insertBefore(nw, it.firstElementChild);
+        init();
+        document.getElementById('IDE-Setting').addEventListener('click', OpenTheWindow);
+    }
+    function init() {
+        var body = document.body;
+        var overlay = document.createElement('div');
+        overlay.setAttribute('id', 'overlay');
+        overlay.classList.add('overlay');
+        body.appendChild(overlay);
+        var modal = document.createElement('div');
+        modal.setAttribute('id', 'modal');
+        modal.classList.add('modal');
+        var close = document.createElement('span');
+        close.classList.add('setting-close');
+        close.innerHTML = '&times;';
+        modal.appendChild(close);
+        modal.innerHTML += '<h2>设置窗口</h2>';
+        var theme = document.createElement('button');
+        theme.innerHTML = '切换主题';
+        theme.classList.add('setting-button');
+        modal.append(theme);
+        body.appendChild(modal);
+        const styleSheet = document.createElement('style');
+        styleSheet.setAttribute('id', 'Setting-Style');
+        document.head.appendChild(styleSheet);
+        styleSheet.sheet.insertRule('.overlay { display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 1000; }', 0);
+        styleSheet.sheet.insertRule('.modal { display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 300px; background-color: white; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); z-index: 1001; }', 0);
+        styleSheet.sheet.insertRule('.setting-close { position: absolute; top: 10px; right: 10px; cursor: pointer; font-size: 20px; font-weight: bold; color: red; }', 0);
+    }
+    function switchThemeMode() {
+        let ret = localStorage.getItem('DE_aemmprty_darkmode');
+        if (ret == 0) {
+            localStorage.setItem('DE_aemmprty_darkmode', 1);
+            var it = document.getElementById('Lightmode-Style');
+            it.remove();
+            DarkMode();
+        } else {
+            localStorage.setItem('DE_aemmprty_darkmode', 0);
+            it = document.getElementById('Darkmode-Style');
+            it.remove();
+            LightMode();
+        }
+    }
+    function closeModal() {
+        console.log('close Modal!');
+        const overlay = document.getElementById('overlay');
+        const modal = document.getElementById('modal');
+        overlay.style.display = 'none';
+        modal.style.display = 'none';
+        //location.reload(true);
+    }
+    function OpenTheWindow() {
+        console.log('open the window');
+        const overlay = document.getElementById('overlay');
+        const modal = document.getElementById('modal');
+        const closeButton = document.getElementsByClassName('setting-close')[0];
+        const theme = document.getElementsByClassName('setting-button')[0];
+        overlay.style.display = 'block';
+        modal.style.display = 'block';
+        console.log(closeButton.innerHTML);
+        closeButton.addEventListener('click', closeModal);
+        theme.addEventListener('click', switchThemeMode);
     }
     template = localStorage.getItem("DE_aemmprty_template");
     console.log(template);
@@ -137,11 +233,18 @@ var darkmode = 0;
         localStorage.setItem("DE_aemmprty_template", res);
     }
     template = dealStyleProblem(template);
-    DarkMode();
     const intervalId = setInterval(() => {
         if (condition()) {
             myFunction();
             clearInterval(intervalId);
+        }
+    }, 100);
+    DarkMode();
+    Public();
+    const intervalOpen = setInterval(() => {
+        if (Open()) {
+            AddSettingButton();
+            clearInterval(intervalOpen);
         }
     }, 100);
 
